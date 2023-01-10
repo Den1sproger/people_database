d3bd62fcd50d9362d61e7127ee179a56bc5a497b
Den1sproger <114018798+Den1sproger@users.noreply.github.com>
2023-01-11 00:55:34 +0300

    fix: delete spaces in sql

--- create_tables_people.sql
+++ create_tables_people.sql
@@ -2,7 +2,7 @@ USE people;
 
 CREATE TABLE persons
 (
-	id serial PRIMARY KEY,
+    id serial PRIMARY KEY,
     first_name varchar(255) NOT NULL,
     surname varchar(255) NOT NULL,
     biography text NOT NULL
@@ -10,7 +10,7 @@ CREATE TABLE persons
 
 CREATE TABLE reviews
 (
-	id serial PRIMARY KEY,
+    id serial PRIMARY KEY,
     review text NOT NULL,
     person_id int REFERENCES persons(id)
 );
